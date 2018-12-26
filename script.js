a37b5f7a94c8e6b79d0229cd68fda0fe28ebc52b
Sergej Jurchenko <sergyurch@gmail.com>
2018-12-26 21:50:25 +0000

    Add clear event

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let fieldSize = 16;
 makeField(fieldSize);
 
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', function(){
+	let divs = document.querySelectorAll('#field div');
+	for (let i=0; i<divs.length; i++) {
+		divs[i].style.backgroundColor = 'white';
+	}
+});
+
 function makeField(size) {
 	for(let i=1; i<=size*size; i++) {
 		let div = document.createElement('div');
+		div.style.backgroundColor = 'red';
 		let parent = document.getElementById('field');
 		div.style.width = parent.clientWidth / size + 'px';
 		div.style.height = parent.clientHeight / size + 'px';
